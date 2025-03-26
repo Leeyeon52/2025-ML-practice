a951889f2e42f26442280504f07bf111a59d17e9
Leeyeon52 <dsy4907@gmail.com>
2025-03-26 15:59:18 +0900

    Update q3.py

--- ML3/q3.py
+++ ML3/q3.py
@@ -1,49 +1,54 @@
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
+
+from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+from sklearn.datasets import make_blobs
+
 
 """
 1. 지시 사항과 동일한 타원형 분포의 데이터를
    생성합니다.
 """
 # make_blobs()으로 데이터를 생성해보세요.
-X, y = make_blobs(n_samples=300, centers=3, cluster_std=1.0, random_state=42)
+X, y = make_blobs(n_samples=300, centers=3, n_features=2, cluster_std=0.8, random_state=0)
 
 # 데이터의 분포를 변형시키기 위해 transformation을 진행합니다.
-transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+transformation = [[0.60834549, -0.63667341],[-0.40887718,0.85253229]]
 X_aniso = np.dot(X, transformation)
 
-# 데이터 프레임 만들기
-clusterDF = pd.DataFrame(data=X_aniso, columns=['ftr1', 'ftr2'])
+# 데이터 프레임 만들기 
+clusterDF = pd.DataFrame(data = X_aniso, columns= ['ftr1','ftr2'])
 clusterDF['target'] = y
 target_list = np.unique(y)
 
 # 생성된 데이터 시각화
 def data_visualize():
     fig, ax = plt.subplots()
-    plt.title('Data')
+    plt.title('data')
     
     for target in target_list:
         target_cluster = clusterDF[clusterDF['target'] == target]
-        ax.scatter(x=target_cluster['ftr1'], y=target_cluster['ftr2'])
+        ax.scatter(x = target_cluster['ftr1'], y = target_cluster['ftr2'])
     
     fig.savefig("plot.png")
-    plt.show()
-
-
+    
+    
 """
 2. K-Means 클러스터링을 수행하여
    클러스터링 결과를 데이터 프레임 내에 
    저장하는 함수를 완성합니다.
 """
 def K_means():
-    k_means = KMeans(n_clusters=3, random_state=42)  # K=3으로 설정
-    kmeans_label = k_means.fit_predict(clusterDF[['ftr1', 'ftr2']])  # 군집화 수행
     
-    clusterDF['kmeans_label'] = kmeans_label
+    k_means = k_means = KMeans(init='random', n_clusters=3, random_state=0)
+    
+    kmeans_label = k_means.fit_predict(X_aniso)
+    
+    clusterDF['kmeans_label']=kmeans_label
     
     # Kmeans 군집의 중심값을 뽑아 저장합니다.
     center = k_means.cluster_centers_
@@ -56,28 +61,28 @@ def K_means():
     for label in unique_labels:
         label_cluster = clusterDF[clusterDF['kmeans_label'] == label]
         center_x_y = center[label]
-        ax.scatter(x=label_cluster['ftr1'], y=label_cluster['ftr2'])
-        ax.scatter(x=center_x_y[0], y=center_x_y[1], s=70, color='k', marker='$%d$' % label)
+        ax.scatter(x = label_cluster['ftr1'], y = label_cluster['ftr2'])
+        ax.scatter(x = center_x_y[0], y = center_x_y[1], s = 70,color = 'k', marker = '$%d$' % label)
     
     fig.savefig("kmeans_plot.png")
-    plt.show()
 
     print("K-means Clustering")
     print(clusterDF.groupby('target')['kmeans_label'].value_counts())
     
     return kmeans_label
-
-
+    
 """
 3. GMM 클러스터링을 수행하여
    클러스터링 결과를 데이터 프레임 내에 
    저장하는 함수를 완성합니다.
 """
 def GMM():
-    gmm = GaussianMixture(n_components=3, random_state=42)  # GMM 모델 생성
-    gmm_label = gmm.fit_predict(clusterDF[['ftr1', 'ftr2']])  # 군집화 수행
     
-    clusterDF['gmm_label'] = gmm_label
+    gmm = GaussianMixture(n_components=3, random_state=0)
+    
+    gmm_label = gmm.fit_predict(X_aniso)
+    
+    clusterDF['gmm_label']=gmm_label
     
     unique_labels = np.unique(gmm_label)
     
@@ -87,22 +92,19 @@ def GMM():
     
     for label in unique_labels:
         label_cluster = clusterDF[clusterDF['gmm_label'] == label]
-        ax.scatter(x=label_cluster['ftr1'], y=label_cluster['ftr2'])
+        plt.scatter(x = label_cluster['ftr1'], y = label_cluster['ftr2'])
     
     fig.savefig("gmm_plot.png")
-    plt.show()
     
     print("Gaussian Mixture Model")
     print(clusterDF.groupby('target')['gmm_label'].value_counts())
     
     return gmm_label
 
-
 def main():
-    data_visualize()  # 데이터 시각화
-    K_means()  # KMeans 클러스터링
-    GMM()  # GMM 클러스터링
-
+    data_visualize()
+    K_means()
+    GMM()
 
 if __name__ == "__main__":
     main()
